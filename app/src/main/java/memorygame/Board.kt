336df450708f7936f666a4ea1cc7bcbbfb989ddb
AsherW13 <aweitz@zagmail.gonzaga.edu>
2024-10-03 16:19:52 -0700

    big board, lots of random cards

--- app/src/main/java/memorygame/Board.kt
+++ app/src/main/java/memorygame/Board.kt
@@ -3,13 +3,17 @@ package memorygame
 class Board( ) {
     //Constructor to create board of different sizes.
     // Assign
-    private val cards: Array<Card> = arrayOf<Card>(Card('c', "a"),
-        Card('h', "10"),
-        Card('h', "10"),
-        Card('d', "3"),
-        Card('c', "a"),
-        Card('d', "3"))
+    private val randomCards = arrayOf<Card>(Card('c', "a"), Card('h', "10"), Card('d', "3"))
+    private var cards: Array<Card> = Array<Card>(6) {_ -> Card(' ', " ")}
 
+    // generate 6 cards
+    init {
+        for (i in 0..2) {
+            cards[2 * i] = randomCards.random()
+            cards[(2 * i) + 1] = cards[2*i]
+        }
+        cards.shuffle()
+    }
     fun getValueAt(index:Int): Card {
         return cards[index]
     }
